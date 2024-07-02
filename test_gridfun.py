e04820d95dd7d09ef7e044f34a46ff2e4a5a9acd
Phil Hunt <calihunlax@gmail.com>
2024-07-02 12:59:22 +0100

    new gridfun test

--- test_gridfun.py
+++ test_gridfun.py
@@ -18,7 +18,19 @@ class T_setSquares(lintest.TestCase):
         g = Grid("111/111/111")
         g2 = setSquares(g, 2) # set all squares to 2
         r = g2.lineStr()
-        self.assertSame(r, "222/222/222", "all squares now 2")
+        self.assertSame(r, "222/222/222", "all squares 1->2")
+
+        g3 = Grid("1234/5009")
+        g4 = setSquares(g3, 8) # set all squares to 8
+        r = g4.lineStr()
+        self.assertSame(r, "8888/8888", "all squares ?->8")
+
+    def test_center_conditional(self):
+        """ change the square at the center """
+        g = Grid("01234/56789")
+        g2 = setSquares(g, 2, center=[3,6]) # set all squares to 2 if 3 or 6
+        r = g2.lineStr()
+        self.assertSame(r, "01224/52789", "all squares 3,6->2")
 
 #---------------------------------------------------------------------
 
